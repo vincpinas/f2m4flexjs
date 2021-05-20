23dec686b21c3dfe87ac6cadfc148a91ff5feb25
vincpinas <70635677+vincpinas@users.noreply.github.com>
2021-05-20 10:24:35 +0200

    :blue_heart: les 2

---
+++ JavaScript/var_dump.js
@@ -0,0 +1,4 @@
+export let achtergronden = document.getElementsByClassName('achtergrond');
+export let voorgronden = document.getElementsByClassName('voorgrond');
+export let achtergrondkleuren = ['green', 'blue', 'white'];
+export let voorgrondkleuren = ['orange', 'yellow', 'black'];
